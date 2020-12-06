d65df6887f64e6a26f3af55c592dedf19418d114
Marcelina <m.oscislowska@gmail.com>
2020-12-06 19:42:59 +0100

    Add hello.hbs

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use((req, res, next) => {
   res.show = (name) => {
@@ -30,6 +33,10 @@ app.get('/info', (req, res) => {
 
 app.get('/history', (req, res, next) => {
   res.show('history.html');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.use((req, res) => {
